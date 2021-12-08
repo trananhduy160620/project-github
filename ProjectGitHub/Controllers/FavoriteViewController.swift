ff64eb3e44112e4b36db7d61415300d0f8a4dddf
trananhduy <tanhduy160620@gmail.com>
2021-12-09 00:34:22 +0700

    [projectgithub] - #06 build favorite screen

---
+++ ProjectGitHub/Controllers/FavoriteViewController.swift
@@ -0,0 +1,21 @@
+//
+//  FavoriteViewController.swift
+//  ProjectGitHub
+//
+//  Created by duytran on 12/9/21.
+//
+
+import UIKit
+
+class FavoriteViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+    
+    private func setupUI() {
+        // write code here
+    }
+}
