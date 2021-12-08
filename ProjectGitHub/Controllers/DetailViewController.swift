d08ecfc90db1d26f5b1e6267684ff2a83b3d35da
trananhduy <tanhduy160620@gmail.com>
2021-12-09 00:20:16 +0700

    [projectgithub] - #02 build detail screen, get data from main screen, setup UI

---
+++ ProjectGitHub/Controllers/DetailViewController.swift
@@ -0,0 +1,24 @@
+//
+//  DetailViewController.swift
+//  ProjectGitHub
+//
+//  Created by duytran on 12/9/21.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+    private func setupUI() {
+        // write code here
+    }
+    
+    private func getDataFromMainViewController() {
+        // write code here
+    }
+}
