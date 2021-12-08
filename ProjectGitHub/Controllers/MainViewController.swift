ea3656d3dc71ee71afc2ec9616cb0566924b8cc5
trananhduy <tanhduy160620@gmail.com>
2021-12-09 00:09:52 +0700

    [projectgithub] - initialized project

---
+++ ProjectGitHub/Controllers/MainViewController.swift
@@ -0,0 +1,15 @@
+//
+//  MainViewController.swift
+//  ProjectGitHub
+//
+//  Created by duytran on 12/9/21.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
