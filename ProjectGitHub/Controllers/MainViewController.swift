9b0e729ba0d03966531a2874ba185d4d3f49b13b
trananhduy <tanhduy160620@gmail.com>
2021-12-10 09:48:27 +0700

    add commit

--- ProjectGitHub/Controllers/MainViewController.swift
+++ ProjectGitHub/Controllers/MainViewController.swift
@@ -18,6 +18,7 @@ class MainViewController: UIViewController {
     private func setupNavigationBar() {
         // write code here
         // write code here
+        // write code here
     }
     
     private func loadDataCategory() {
